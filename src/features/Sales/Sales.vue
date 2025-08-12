983fdde8f2be90de806805693f81f33061a97d88
gqtpe <work.temirtas@gmail.com>
2025-08-12 23:47:27 +0500

    feat(sales): sales page initial structure created

---
+++ src/features/Sales/Sales.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import {onMounted} from "vue";
+import {storeToRefs} from 'pinia';
+import {useSales} from "@/features/Sales/salesStore.ts";
+
+
+const incomesStore = useSales();
+const {sales} = storeToRefs(incomesStore);
+const {getSales} = incomesStore;
+onMounted(async () => {
+  await getSales({
+    dateFrom: '2025-08-11',
+    dateTo: '2025-08-11',
+    limit: 10,
+    page: 1,
+  });
+});
+</script>
+
+<template>
+  Incomes Page
+  {{ JSON.stringify(sales) }}
+</template>
