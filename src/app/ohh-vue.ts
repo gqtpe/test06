b27431a5a46b1287dcdd7d851beda553f09ba7be
gqtpe <work.temirtas@gmail.com>
2025-08-12 23:46:15 +0500

    build: ohh-vue initialization

---
+++ src/app/ohh-vue.ts
@@ -0,0 +1,13 @@
+import {addIcons, OhVueIcon} from "oh-vue-icons";
+import {BiBarChartLineFill, BiClipboard2PulseFill, CoMoney, CoSad, GiCardboardBox} from "oh-vue-icons/icons";
+
+addIcons(
+    CoSad
+    , GiCardboardBox
+    , BiBarChartLineFill
+    , BiClipboard2PulseFill
+    , CoMoney
+);
+
+
+export default OhVueIcon;
