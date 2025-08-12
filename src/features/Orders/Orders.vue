6941de5e2f6d93c1e90f9eb1880b217bede4939a
gqtpe <work.temirtas@gmail.com>
2025-08-12 23:41:06 +0500

    feat(orders): add initial page structure

--- src/features/Orders/Orders.vue
+++ src/features/Orders/Orders.vue
@@ -1,63 +1,115 @@
 <script setup lang="ts">
-import {onBeforeMount, onMounted, ref, watch} from "vue";
-import {storeToRefs} from 'pinia';
+import {onBeforeMount, ref, watch} from "vue";
+import {storeToRefs} from "pinia";
 import {useOrders} from "@/features/Orders/ordersStore.ts";
 import dayjs from "dayjs";
+import {createColumnHelper, getCoreRowModel, getSortedRowModel, type Table, useVueTable,} from "@tanstack/vue-table";
+import TableC from "@/components/Table.vue"
+import type {Order, OrdersPageFilters} from "@/features/Orders/types/types.ts"
+import {useRoute, useRouter} from "vue-router";
 
-type Filters = {
-  dateFrom: string
-  dateTo: string
-  page: number
-  limit: number
-}
-
-const incomesStore = useOrders();
-const {orders} = storeToRefs(incomesStore);
-const {getOrders} = incomesStore;
-const filters = ref<Filters>({
-  dateFrom: '',
-  dateTo: '',
+const ordersStore = useOrders()
+const {orders} = storeToRefs(ordersStore)
+const {getOrders} = ordersStore
+const router = useRouter()
+const route = useRoute()
+const filters = ref<OrdersPageFilters>({
+  dateFrom: "",
+  dateTo: "",
   limit: 10,
   page: 1,
-})
+});
+
 
 onBeforeMount(() => {
-  const today = dayjs();
-  filters.value.dateFrom = today.clone().subtract(1, "day").format("YYYY-MM-DD");
-  filters.value.dateTo = today.format("YYYY-MM-DD");
-})
 
-onMounted(() => {
-  watch(filters, (newValue) => {
-    getOrders(newValue)
-  },{deep:true, immediate:true})
-})
+  if (!filters.value.dateFrom || !filters.value.dateTo) {
+    const today = dayjs();
+    filters.value.dateFrom = filters.value.dateFrom || today.clone().subtract(1, "day").format("YYYY-MM-DD");
+    filters.value.dateTo = filters.value.dateTo || today.format("YYYY-MM-DD");
+  }
+
+
+  //getting filters from query params if it exists
+  for (const key of Object.keys(route.query)) {
+    if (key in filters.value) {
+      const typedKey = key as keyof OrdersPageFilters;
+      const value = route.query[typedKey];
+      if (typedKey === "page" || typedKey === "limit") {
+        filters.value[typedKey] = Number(value) as OrdersPageFilters[typeof typedKey];
+      } else {
+        if(dayjs(String(value), 'YYYY-MM-DD', true).isValid()){
+          filters.value[typedKey] = String(value) as OrdersPageFilters[typeof typedKey];
+        }
+
+      }
+    }
+  }
+
+
+});
+watch(filters, (newValue) => {
+  router.push({
+    query: {
+      ...newValue
+    }
+  })
+  getOrders(newValue);
+}, {deep: true});
+
+//table
+const columnHelper = createColumnHelper<Order>();
+
+const columns = [
+  columnHelper.accessor("g_number", {header: "G Number"}),
+  columnHelper.accessor((cell)=>dayjs(cell.date).format('YYYY/MM/DD'), {header: "Date"}),
+  columnHelper.accessor("supplier_article", {header: "Article"}),
+  columnHelper.accessor("oblast", {header: "Region"}),
+  columnHelper.accessor("brand", {header: "Brand"}),
+  columnHelper.accessor("warehouse_name", {header: "Warehouse"}),
+  columnHelper.accessor("total_price", {header: "Price"}),
+];
+
+// Табличный инстанс
+const table: Table<Order> = useVueTable({
+  data: orders,
+  columns,
+  getCoreRowModel: getCoreRowModel(),
+  getSortedRowModel: getSortedRowModel(),
+});
 </script>
 
 <template>
   <div class="orders-page">
+    <!-- Фильтры -->
     <div class="filters">
-      <input class="date-input" type="date" v-model="filters.dateFrom">
-      <input class="date-input" type="date" v-model="filters.dateTo">
+      <input class="date-input" type="date" v-model="filters.dateFrom"/>
+      <input class="date-input" type="date" v-model="filters.dateTo"/>
     </div>
+
+    <!-- Таблица -->
     <div class="orders">
-      {{JSON.stringify(orders)}}
+      <TableC :table="table"/>
+    </div>
+    <div>
+      pagination
+      <button @click="filters.page!==0 && filters.page--"><-</button>
+      <button @click="filters.page++">-></button>
     </div>
   </div>
+
 </template>
-<style>
+
+<style scoped>
 .orders-page {
   width: 100vw;
   height: 100vh;
   overflow: hidden;
 }
-.orders{
-  color: white;
-}
+
 .filters {
   display: flex;
   justify-content: center;
-  width: 100%;
   gap: 1rem;
   padding: 0.5rem;
 }
@@ -67,7 +119,13 @@ onMounted(() => {
   color-scheme: dark;
   padding: 0.5rem;
   border-radius: 0.25rem;
-  display: inline-block;
   border: 1px solid white;
 }
+
+.orders {
+  padding: 0.5rem;
+  overflow-y: auto;
+}
+
+
 </style>
