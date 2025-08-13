ee85fc74da53c7449f1ff20685f226d92df46553
gqtpe <work.temirtas@gmail.com>
2025-08-13 22:08:29 +0500

    refactor: update App import path

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import 'vue-toast-notification/dist/theme-bootstrap.css';
 import './main.css';
-import App from './App.vue'
+import App from '../features/App/App.vue'
 import router from '@/app/router.ts'
 import {useAuth} from "@/features/Auth/authStore.ts";
 import OhVueIcon from "./ohh-vue.ts"
