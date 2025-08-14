a7e57b7ad2c8214562688fcc732593300fd9db06
gqtpe <work.temirtas@gmail.com>
2025-08-14 10:05:56 +0500

    final refactoring

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import Chart from 'chart.js/auto';
-import {onBeforeUnmount, onMounted, ref, useTemplateRef, watch} from "vue";
+import {onBeforeUnmount, ref, useTemplateRef, watch} from "vue";
 import type {Entities, IncomesItem, OrderItem, SaleItem, StockItem} from "@/api/entities.ts";
 
 type Props = { data: Entities }
@@ -8,47 +8,39 @@ const {data} = defineProps<Props>();
 
 const ctx = useTemplateRef<HTMLCanvasElement>('myChart');
 const chartInstance = ref<Chart>();
+
 const selectValueFromEntities = (data: Entities) => {
-  if ('number' in (data[0] || {})) {
-    return (data as IncomesItem[]).map(item => Number(item.quantity));
-  } else if ('discount' in (data[0] || {})) {
-    return (data as StockItem[]).map(item => Number(item.price));
-  } else if ('oblast' in (data[0] || {})) {
-    return (data as OrderItem[]).map(item => Number(item.total_price));
+  if (data[0]?.hasOwnProperty('number')) {
+    return (data as IncomesItem[]).map(item => item.quantity);
+  } else if (data[0]?.hasOwnProperty('discount')) {
+    return (data as StockItem[]).map(item => item.price);
+  } else if (data[0]?.hasOwnProperty('oblast')) {
+    return (data as OrderItem[]).map(item => item.total_price);
   } else {
-    return (data as SaleItem[]).map(item => Number(item.finished_price));
+    return (data as SaleItem[]).map(item => item.finished_price);
   }
 };
-
-
-onMounted(() => {
-  if (ctx.value) {
-    chartInstance.value = new Chart(ctx.value, {
-      type: 'line',
-      data: {
-        labels: data.map(item => item.date),
-        datasets: [{
-          label: 'value',
-          data: selectValueFromEntities(data),
-          borderWidth: 1
-        }]
-      }
-    });
-  }
-});
 
 watch(
     () => data,
     (newData) => {
-      if (chartInstance.value) {
-        chartInstance.value.data.labels = newData.map(item => item.date);
-        chartInstance.value.data.datasets[0].data = selectValueFromEntities(newData);
-        chartInstance.value.update();
+      chartInstance.value?.destroy();
+      if (ctx.value) {
+        chartInstance.value = new Chart(ctx.value, {
+          type: 'line',
+          data: {
+            labels: newData.map(item => item.date),
+            datasets: [{
+              label: 'value',
+              data: selectValueFromEntities(newData),
+              borderWidth: 1
+            }]
+          }
+        });
       }
     },
     {deep: true}
 );
-
 onBeforeUnmount(() => {
   chartInstance.value?.destroy();
 });
