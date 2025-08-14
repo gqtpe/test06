caf7534bb77bb15129d8067eeb3dc389d8708fed
gqtpe <work.temirtas@gmail.com>
2025-08-14 09:01:19 +0500

    feat(charts): Charts component creatd

---
+++ src/components/Chart.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import Chart from 'chart.js/auto';
+import {onMounted, onBeforeUnmount, useTemplateRef, watch, ref} from "vue";
+import type {Entities, IncomesItem, OrderItem, SaleItem, StockItem} from "@/api/entities.ts";
+
+type Props = { data: Entities }
+const {data} = defineProps<Props>();
+
+const ctx = useTemplateRef<HTMLCanvasElement>('myChart');
+const chartInstance = ref<Chart>();
+
+const selectValueFromEntities = (data: Entities) => {
+  if (data[0]?.hasOwnProperty('number')) {
+    return (data as IncomesItem[]).map(item => item.quantity);
+  } else if (data[0]?.hasOwnProperty('discount')) {
+    return (data as StockItem[]).map(item => item.price);
+  } else if (data[0]?.hasOwnProperty('oblast')) {
+    return (data as OrderItem[]).map(item => item.total_price);
+  } else {
+    return (data as SaleItem[]).map(item => item.finished_price);
+  }
+};
+
+watch(
+    () => data,
+    (newData) => {
+      chartInstance.value?.destroy();
+      if (ctx.value) {
+        chartInstance.value = new Chart(ctx.value, {
+          type: 'bar',
+          data: {
+            labels: newData.map(item => item.date),
+            datasets: [{
+              label: 'Acquisitions by year',
+              data: selectValueFromEntities(newData),
+              borderWidth: 1
+            }]
+          }
+        });
+      }
+    },
+    {deep: true}
+);
+onBeforeUnmount(() => {
+  chartInstance.value?.destroy();
+});
+</script>
+
+<template>
+  <canvas ref="myChart"></canvas>
+</template>
