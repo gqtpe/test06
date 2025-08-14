494f38ecf5c04a7b26353affa84ba2567d0439ee
gqtpe <work.temirtas@gmail.com>
2025-08-14 09:54:41 +0500

    final refactoring

--- src/features/App/Home.vue
+++ src/features/App/Home.vue
@@ -3,9 +3,12 @@
 </script>
 
 <template>
-  Hello Page
+  <h1 class="hero font-main text-center text-white">test 06</h1>
 </template>
 
 <style scoped>
-
+  .hero{
+    font-variant: all-small-caps;
+    font-size: clamp(2rem,8vw, 4rem);
+  }
 </style>
